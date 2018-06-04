8092c99af4b0f8532c825bd49acab903f9e26b0d
ludovicm67 <ludovicmuller1@gmail.com>
2018-06-04 18:04:43 +0200

    display all clients with all contacts

--- application/controllers/Clients.php
+++ application/controllers/Clients.php
@@ -3,7 +3,21 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Clients extends CI_Controller
 {
-  public function index() {
-    $this->load->view('clients');
+  public function index()
+  {
+    $clientsDB = $this->db->get('sellsy_clients')->result();
+    $clients = [];
+    foreach ($clientsDB as $k => $c) {
+      $clientsDB[$k]->contacts = [];
+      $clients[$c->id] = $clientsDB[$k];
+    }
+    $contacts = $this->db->get('sellsy_contacts')->result();
+    foreach ($contacts as $k => $c) {
+      if (isset($clients[$c->thirdid])) {
+        $clients[$c->thirdid]->contacts[] = $c;
+      }
+    }
+
+    $this->load->view('clients', ['clients' => $clients]);
   }
 }
--- application/views/clients.php
+++ application/views/clients.php
@@ -6,6 +6,41 @@ ob_start();
 <h1 class="mt-5">Liste des clients</h1>
 <p class="lead">Page listant les différents clients</p>
 
+<ul>
+  <?php foreach ($clients as $client): ?>
+  <li>
+    <ul>
+      <li><strong>Nom complet :</strong> <?php echo $client->fullname; ?></li>
+      <li>
+        <strong>Contacts :</strong>
+        <ul>
+          <?php foreach ($client->contacts as $contact): ?>
+          <li>
+            <ul>
+              <?php if (!empty($contact->fullname)): ?>
+                <li><strong>Nom complet :</strong> <?php echo $contact->fullname; ?></li>
+              <?php endif; ?>
+              <?php if (!empty($contact->email)): ?>
+                <li><strong>Email :</strong> <?php echo $contact->email; ?></li>
+              <?php endif; ?>
+              <?php if (!empty($contact->tel)): ?>
+                <li><strong>Téléphone :</strong> <?php echo $contact->tel; ?></li>
+              <?php endif; ?>
+              <?php if (!empty($contact->mobile)): ?>
+                <li><strong>Mobile :</strong> <?php echo $contact->mobile; ?></li>
+              <?php endif; ?>
+              <?php if (!empty($contact->position)): ?>
+                <li><strong>Position :</strong> <?php echo $contact->position; ?></li>
+              <?php endif; ?>
+            </ul>
+          </li>
+          <?php endforeach; ?>
+        </ul>
+      </li>
+    </ul>
+  </li>
+<?php endforeach; ?>
+</ul>
 
 <?php
 $content = ob_get_clean();
