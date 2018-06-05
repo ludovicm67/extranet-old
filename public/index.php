4323d86606bef36282c7117d3ce2fc98d41e8488
ludovicm67 <ludovicmuller1@gmail.com>
2018-06-05 09:56:02 +0200

    no limit for xdebug in local

--- public/index.php
+++ public/index.php
@@ -70,6 +70,9 @@ switch (ENVIRONMENT) {
   case 'development':
     error_reporting(-1);
     ini_set('display_errors', 1);
+    ini_set('xdebug.var_display_max_depth', -1);
+    ini_set('xdebug.var_display_max_children', -1);
+    ini_set('xdebug.var_display_max_data', -1);
     break;
 
   case 'testing':
