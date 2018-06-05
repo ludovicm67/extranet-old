95f6b9c2ad3b0565c9169838851019e37cd0c265
ludovicm67 <ludovicmuller1@gmail.com>
2018-06-05 10:17:23 +0200

    added lot of fields

--- database/sellsy_contacts.sql
+++ database/sellsy_contacts.sql
@@ -7,23 +7,23 @@ DROP TABLE IF EXISTS `sellsy_contacts`;
 
 CREATE TABLE `sellsy_contacts` (
   `id` int(11) NOT NULL,
-  `pic` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `forename` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tel` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `mobile` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `civil` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `position` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `birthdate` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pic` varchar(255) COLLATE utf8mb4_unicode_ci,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci,
+  `forename` varchar(255) COLLATE utf8mb4_unicode_ci,
+  `tel` varchar(255) COLLATE utf8mb4_unicode_ci,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci,
+  `mobile` varchar(255) COLLATE utf8mb4_unicode_ci,
+  `civil` varchar(255) COLLATE utf8mb4_unicode_ci,
+  `position` varchar(255) COLLATE utf8mb4_unicode_ci,
+  `birthdate` varchar(255) COLLATE utf8mb4_unicode_ci,
   `thirdid` int(11),
-  `peopleid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `fullName` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `peopleid` int(11),
+  `fullName` varchar(255) COLLATE utf8mb4_unicode_ci,
   `corpid` int(11),
-  `formatted_tel` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `formatted_mobile` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `formatted_fax` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `formatted_birthdate` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `formatted_tel` varchar(255) COLLATE utf8mb4_unicode_ci,
+  `formatted_mobile` varchar(255) COLLATE utf8mb4_unicode_ci,
+  `formatted_fax` varchar(255) COLLATE utf8mb4_unicode_ci,
+  `formatted_birthdate` varchar(255) COLLATE utf8mb4_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 ALTER TABLE `sellsy_contacts` ADD PRIMARY KEY (`id`);
