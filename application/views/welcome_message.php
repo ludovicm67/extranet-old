6a2ebc15d5da1ac659ed113bf58e73ca748a192e
ludovicm67 <ludovicmuller1@gmail.com>
2018-06-05 11:02:09 +0200

    added explainations on homepage about all differents links

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -8,14 +8,14 @@ ob_start();
 
 <h2>Pages pour tester</h2>
 <ul>
-  <li><a href="/clients" target="_blank">/clients</a></li>
+  <li><a href="/clients" target="_blank">/clients</a> : liste les différents clients et leurs contacts associés</li>
 </ul>
 
 <h2>Pages à appeler depuis des tâches cron</h2>
 <ul>
-  <li><a href="/cron/reset_database" target="_blank">/cron/reset_database</a></li>
-  <li><a href="/cron/sellsy_clients" target="_blank">/cron/sellsy_clients</a></li>
-  <li><a href="/cron/sellsy_contacts" target="_blank">/cron/sellsy_contacts</a></li>
+  <li><a href="/cron/reset_database" target="_blank">/cron/reset_database</a> : à appeler pour vider complètement la base de données (pour toutes les tables ayant des informations de Sellsy) et reconstruire l'architecture des différentes tables</li>
+  <li><a href="/cron/sellsy_clients" target="_blank">/cron/sellsy_clients</a> : à appeler pour récupérer l'ensemble des clients avec leurs contacts associés</li>
+  <li><a href="/cron/sellsy_contacts" target="_blank">/cron/sellsy_contacts</a> : à appeler pour mettre à jour les informations sur l'ensemble des contacts</li>
 </ul>
 
 <?php
