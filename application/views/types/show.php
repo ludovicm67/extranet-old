2ca27a678cd1aa7fd93a17d4dd2447285d67e5ff
ludovicm67 <ludovicmuller1@gmail.com>
2018-06-06 19:30:41 +0200

    fixed type view

--- application/views/types/show.php
+++ application/views/types/show.php
@@ -4,23 +4,11 @@ ob_start();
 ?>
 
 <h1 class="mt-5">
-  <?php echo $tag->name; ?>
-  <?php if (!empty($this->input->get('value'))): ?>
-    <small><?php echo strip_tags($this->input->get('value')); ?></small>
-    <a class="btn btn-outline-secondary" href="/tag/<?php echo $tag->id; ?>" role="button">Enlever le filtre</a>
-  <?php endif; ?>
-  <a class="btn btn-outline-primary" href="/tag/edit/<?php echo $tag->id; ?>" role="button">Modifier</a>
-  <a class="btn btn-outline-danger" href="/tag/delete/<?php echo $tag->id; ?>" role="button">Supprimer</a>
+  <?php echo $type->name; ?>
+  <a class="btn btn-outline-primary" href="/type/edit/<?php echo $type->id; ?>" role="button">Modifier</a>
+  <a class="btn btn-outline-danger" href="/type/delete/<?php echo $type->id; ?>" role="button">Supprimer</a>
 </h1>
-<p class="lead">Affichage des projets utilisant ce tag</p>
-
-<ul>
-  <?php foreach ($tag->projects as $project): ?>
-  <li>
-    <a href="/project/<?php echo $project->id; ?>"><?php echo $project->name; ?></a>
-  </li>
-<?php endforeach; ?>
-</ul>
+<p class="lead">Affichage des contacts de ce type</p>
 
 <?php
 $content = ob_get_clean();
