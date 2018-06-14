5f106093271bdbe81e39b84b87b243431f095494
ludovicm67 <ludovicmuller1@gmail.com>
2018-06-14 10:58:08 +0200

    removed tables in search results

--- application/views/search.php
+++ application/views/search.php
@@ -34,74 +34,36 @@ ob_start();
 
 <?php if (!empty($results->contacts)): ?>
 <h2>Contacts</h2>
-<table class="table table-hover">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">Nom</th>
-      <th scope="col">Actions</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($results->contacts as $contact): ?>
-    <tr>
-      <td><a href="/contact/<?php echo $contact->id; ?>"><?php echo $contact->name; ?></a></td>
-      <td>
-        <a href="/contact/edit/<?php echo $contact->id; ?>">Modifier</a>
-        -
-        <a href="/contact/delete/<?php echo $contact->id; ?>">Supprimer</a>
-      </td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+<ul>
+  <?php foreach ($results->contacts as $contact): ?>
+    <li>
+      <a href="/contact/<?php echo $contact->id; ?>"><?php echo $contact->name; ?></a>
+    </li>
+  <?php endforeach; ?>
+</ul>
 <?php endif; ?>
 
 <?php if (!empty($results->users)): ?>
 <h2>Utilisateurs</h2>
-<table class="table table-hover">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">Nom</th>
-      <th scope="col">Adresse mail</th>
-      <th scope="col">Rôle</th>
-      <th scope="col">Administrateur ?</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($results->users as $user): ?>
-    <tr>
-      <td><a href="/user/<?php echo $user->id; ?>"><?php echo $user->firstname; ?> <?php echo $user->lastname; ?></a></td>
-      <td><a href="mailto:<?php echo htmlspecialchars($user->mail); ?>"><?php echo $user->mail; ?></a></td>
-      <td><?php echo ($user->role) ? $user->role : 'Aucun rôle'; ?></td>
-      <td><?php echo ($user->is_admin) ? 'Oui' : 'Non'; ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+<ul>
+  <?php foreach ($results->users as $user): ?>
+  <li>
+    <a href="/user/<?php echo $user->id; ?>"><?php echo $user->firstname; ?> <?php echo $user->lastname; ?></a>
+    (<a href="mailto:<?php echo htmlspecialchars($user->mail); ?>"><?php echo $user->mail; ?></a>)
+  </li>
+  <?php endforeach; ?>
+</ul>
 <?php endif; ?>
 
 <?php if (!empty($results->tags)): ?>
 <h2>Tags</h2>
-<table class="table table-hover">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">Nom</th>
-      <th scope="col">Actions</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($results->tags as $tag): ?>
-    <tr>
-      <td><a href="/tag/<?php echo $tag->id; ?>"><?php echo $tag->name; ?></a></td>
-      <td>
-        <a href="/tag/edit/<?php echo $tag->id; ?>">Modifier</a>
-        -
-        <a href="/tag/delete/<?php echo $tag->id; ?>">Supprimer</a>
-      </td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+<ul>
+  <?php foreach ($results->tags as $tag): ?>
+  <li>
+    <a href="/tag/<?php echo $tag->id; ?>"><?php echo $tag->name; ?></a>
+  </li>
+  <?php endforeach; ?>
+</ul>
 <?php endif; ?>
 
 <?php endif; ?>
